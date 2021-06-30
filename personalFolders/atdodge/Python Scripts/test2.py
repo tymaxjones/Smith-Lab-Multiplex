01dab9ef0cd71e6e44b37dc47c74c3f2a74ac580
Aaron Dodge <dod17005@byui.edu>
2021-06-30 15:04:40 -0600

    Designs primers based on a FASTA file upload

--- personalFolders/atdodge/Python Scripts/test2.py
+++ personalFolders/atdodge/Python Scripts/test2.py
@@ -1,4 +1,25 @@
 import primer3
+from Bio import SeqIO
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+
+Tk().withdraw()
+test_file = askopenfilename()
+file_parsed = SeqIO.parse(open(test_file, mode='r'), 'fasta')
+
+for seq_record in file_parsed:
+	print(primer3.bindings.designPrimers(
+		{
+			'SEQUENCE_ID': seq_record.id,
+			'SEQUENCE_TEMPLATE': str(seq_record.seq)
+		},
+		{
+			'PRIMER_OPT_SIZE': 18,
+			'PRIMER_MAX_SIZE': 25,
+			'PRIMER_OPT_TM': 60.0
+		}
+	))
 
 print(primer3.bindings.designPrimers(
 	{
@@ -8,6 +29,7 @@ print(primer3.bindings.designPrimers(
 	{
 		'PRIMER_OPT_SIZE': 18,
 		'PRIMER_MAX_SIZE': 25,
-		'PRIMER_OPT_TM': 60.0
+		'PRIMER_OPT_TM': 60.0,
+		'PRIMER_NUM_RETURN': 7
 	}
 ))
